2b886eef6641625ccf03438a734f0186c31221ae
Maaz <maazkh2700@gmail.com>
2023-06-14 17:50:58 +0530

    my new pokemon website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Topbar from './components/Topbar';
+import Pokeball from './images/pokeball.png'
+import Pokedex from './images/Pokedex_logo.png'
+
+import Main from "./components/Main"
+import './components/style.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <>
+    <h1>
+      Please Choose a pokemon
+    </h1>
+    <div className='balll'>
+    <img  src={Pokeball} alt='' />
+    <img className='poked' src={Pokedex} />
     </div>
+    <Main />
+   
+    </>
+   
+    
   );
 }
 
---
+++ src/components/Topbar.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import './Topbar.css'
+import { useState } from 'react'
+import axios from 'axios';
+function Topbar() {
+ 
+   const[pokemonName, setPokemonName] = useState("");
+   const [pokemonChosen, setPokemonChosen] = useState(false); 
+    const[pokemon, setPokemon] = useState ({
+      name: "",
+           species: "",
+          img: "",
+        hp: "",
+        attack:"",
+      defense: "",
+      type: "",
+    }); 
+
+    const searchPokemon =() =>{
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(
+      (response)=>{ 
+        setPokemon({name: pokemonName,
+           species: response.data.species.name,
+          img: response.data.sprites.front_default,
+        hp: response.data.stats[0].base_stat,
+        attack: response.data.stats[1].base_stat,
+      defense: response.data.stats[2].base_stat,
+      type: response.data.type[0].type.name,
+     });
+  setPokemonChosen(true);
+    })
+    }
+  return (
+    <div className='Topbar'>
+    <div className='Titlesection'>
+      <h1>Pokemon Status</h1>
+      <input type='text' onChange={(event) =>{setPokemonName(event.target.value);
+      }} />
+      <button onClick={searchPokemon}>Search Pokemon</button>
+    </div>
+    <div className='Displaysection' >{!pokemonChosen ? (<h1>Please Choose  a pokemon</h1>):<h1>{pokemonName}</h1>}</div>
+    </div>
+  )
+}
+
+export default Topbar
